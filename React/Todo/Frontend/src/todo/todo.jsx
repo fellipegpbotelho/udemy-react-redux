df1b19337c1d48ada5564d83ba7440865116be82
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-09-23 18:56:45 -0300

    - (REACT-TODO-FRONTEND): Componentes TodoForm e TodoList;

--- React/Todo/Frontend/src/todo/todo.jsx
+++ React/Todo/Frontend/src/todo/todo.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
+
 import PageHeader from "../template/pageHeader"
 
 export default class Todo extends Component {
@@ -7,6 +10,8 @@ export default class Todo extends Component {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
