09fc5bb69cb499a52407df068001efce16368f00
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-09-23 18:51:44 -0300

    - (REACT-TODO-FRONTEND): Componente Page Header;

--- React/Todo/Frontend/src/about/about.jsx
+++ React/Todo/Frontend/src/about/about.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import PageHeader from "../template/pageHeader"
+
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós" />
+    <h2>Nossa História</h2>
+    <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14... Por que é que não?</p>
+    <h2>Missão</h2>
+    <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14... Por que é que não?</p>
+    <h2>Visão</h2>
+    <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14... Por que é que não?</p>
+    <h2>Contato</h2>
+    <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14... Por que é que não?</p>
   </div>
 );
