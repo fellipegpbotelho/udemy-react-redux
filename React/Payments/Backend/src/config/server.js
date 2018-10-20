610f573ba775eb25d829fb00d053c0131ce4a03e
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-20 10:04:07 -0300

    Aplicando middleware query parse int

--- React/Payments/Backend/src/config/server.js
+++ React/Payments/Backend/src/config/server.js
@@ -2,6 +2,7 @@ const port = 3003;
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const queryParser = require('express-query-int');
 const allowCors = require('./cors');
 
 const server = express();
@@ -9,6 +10,7 @@ const server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser);
 
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${port}`);
