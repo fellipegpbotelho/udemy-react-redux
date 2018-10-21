17b1f9d40aa1788f7083ced1c1f7695e6cba1036
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-21 11:54:26 -0300

    Construindo componente TabHeader

--- React/Payments/Frontend/src/BillingCycle/BillingClycle.jsx
+++ React/Payments/Frontend/src/BillingCycle/BillingClycle.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Content from '../common/template/Content';
 import ContentHeader from '../common/template/ContentHeader';
 import Tabs from '../common/tab/Tabs';
+import TabHeader from '../common/tab/TabHeader';
 import TabsHeader from '../common/tab/TabsHeader';
 import TabsContent from '../common/tab/TabsContent';
 
@@ -13,7 +14,12 @@ class BillingCycle extends Component {
         <ContentHeader title="Ciclos de pagamento" small="Cadastro" />
         <Content>
           <Tabs>
-            <TabsHeader />
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
             <TabsContent />
           </Tabs>
         </Content>
