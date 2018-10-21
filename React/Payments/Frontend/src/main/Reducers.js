b8987dec5da59323f4f98b2d4a2590c1d795ea14
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-21 12:02:52 -0300

    Construindo ActionCreator e Reducer para as tabs

--- React/Payments/Frontend/src/main/Reducers.js
+++ React/Payments/Frontend/src/main/Reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import DashboardReducer from '../Dashboard/DashboardReducer';
+import TabReducer from '../common/tab/TabReducer';
 
 const RootReducer = combineReducers({
   dashboard: DashboardReducer,
+  tabs: TabReducer,
 });
 
 export default RootReducer;
