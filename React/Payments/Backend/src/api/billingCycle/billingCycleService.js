13f07113c906e724a5d0e6520a3a98fd10088bca
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-21 11:15:46 -0300

    Corrigindo bug em /summary

--- React/Payments/Backend/src/api/billingCycle/billingCycleService.js
+++ React/Payments/Backend/src/api/billingCycle/billingCycleService.js
@@ -24,7 +24,7 @@ BillingCycle.route('summary', (req, res) => {
       $group: { _id: null, credit: { $sum: '$credit' }, debt: { $sum: '$debt' } },
     },
     {
-      $project: { id: 0, credit: 1, debt: 1 },
+      $project: { _id: 0, credit: 1, debt: 1 },
     },
     (error, result) => {
       if (error) {
