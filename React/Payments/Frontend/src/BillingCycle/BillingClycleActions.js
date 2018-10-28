c15e437225d3e1a9d41484e52e5a238ddad0c5b0
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-27 23:04:14 -0300

    Integrando BillingCycleList com Redux

--- React/Payments/Frontend/src/BillingCycle/BillingClycleActions.js
+++ React/Payments/Frontend/src/BillingCycle/BillingClycleActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3003/api';
 
 export function getList() {
-  const request = axios.get(`${BASE_URL}/billingClycles`);
+  const request = axios.get(`${BASE_URL}/billingCycles`);
   return {
     type: 'BILLING_CYCLES_FETCHED',
     payload: request,
--- React/Payments/Frontend/src/BillingCycle/BillingClycleList.jsx
+++ React/Payments/Frontend/src/BillingCycle/BillingClycleList.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getList } from './BillingClycleActions';
 
 class BillingCycleList extends Component {
+  componentWillMount() {
+    this.props.getList();
+  }
+
   render() {
+    console.log(this.props.list);
     return (
       <div>
         <table className="table">
@@ -19,4 +28,13 @@ class BillingCycleList extends Component {
   }
 }
 
-export default BillingCycleList;
+const mapStateToProps = state => ({
+  list: state.billingCycle.list,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(BillingCycleList);
