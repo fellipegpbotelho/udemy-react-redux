51280ba247dc55ce50c42f4b1c04b0243b5c3e0f
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-28 09:29:52 -0300

    Melhorias na inclusão do ciclo de pagamento

--- React/Payments/Frontend/src/index.jsx
+++ React/Payments/Frontend/src/index.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import promise from 'redux-promise';
+import multi from 'redux-multi';
+import thunk from 'redux-thunk';
 
 import reducers from './main/Reducers';
 
 import App from './main/App';
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(promise)(createStore)(reducers, reduxDevTools);
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, reduxDevTools);
 
 const MyMoney = () => (
   <Provider store={store}>
