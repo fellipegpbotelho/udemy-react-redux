4e3272e5408920f1d7a5dcd5c4a07c715d0bc773
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2018-10-27 22:46:13 -0300

    Criando action getList e o Reducer

--- React/Payments/Frontend/src/main/Reducers.js
+++ React/Payments/Frontend/src/main/Reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import DashboardReducer from '../Dashboard/DashboardReducer';
 import TabReducer from '../common/tab/TabReducer';
+import BillingCycleReducer from '../BillingCycle/BillingCycleReducer';
 
 const RootReducer = combineReducers({
   dashboard: DashboardReducer,
   tab: TabReducer,
+  billingCycle: BillingCycleReducer,
 });
 
 export default RootReducer;
